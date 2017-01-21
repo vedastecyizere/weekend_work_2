4b1048a571c4cc2e363ef095f5baa3ddb35c0207
Vedaste Cyizere <revedaste@gmail.com>
2017-01-21 12:12:38 +0200

    Using palindrome method to indicate if a string is a palindrome

--- algo/palindrome.rb
+++ algo/palindrome.rb
@@ -1,7 +1,11 @@
 # Complete the method called palindrome? which should accept a string as a parameter and return a boolean indicated whether the string is a palindrome. A palindrome is a word that reads the same both forwards and backwards. Examples: eye, madam, racecar
 
 def palindrome?(string)
-
+  if string == string.reverse
+    return true
+  else 
+    return false
+  end 
 end
 
 # Driver code - don't touch anything below this line.
